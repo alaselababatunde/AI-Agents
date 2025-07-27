a987ade45e7d2c8ff87552ad2678b93eecabca06
Alasela Babatunde <alaselababatunde10@gmail.com>
2025-07-27 20:47:37 +0100

    Updated

--- Pizza_AI_Agent/vector.py
+++ Pizza_AI_Agent/vector.py
@@ -1,4 +1,4 @@
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
